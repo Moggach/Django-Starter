3f4f2348d3d727833f805dd3ff52af329c8879c6
“Moggach” <acunnane13@gmail.com>
2021-12-22 14:59:25 +0000

    outputting hardcoded list onto home page

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+shows = [
+    {'id': 1, 'name': 'Seinfeld'},
+    {'id': 2, 'name': 'The Simpsons'},
+    {'id': 3, 'name': 'The Thick of It'},
+    {'id': 4, 'name': 'Succession'},
+]
 
 # Create your views here.
 def index(request):
-    return render (request, 'pages/index.html')
+    return render (request, 'pages/index.html', {'shows': shows})
 
 def about(request):
     return render(request, 'pages/about.html')
