2386bb2695a52fb420bc8e1b21a7ac67bfbc7b9b
“Moggach” <acunnane13@gmail.com>
2021-12-22 17:00:49 +0000

    outputting shows data from db onto shows page

--- shows/views.py
+++ shows/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 from .models import Shows
 
 def shows(request):
-    return render(request, 'shows/shows.html')
+    shows = Shows.objects.all()
+    context = {
+      'shows': shows
+    }
+    return render(request, 'shows/shows.html', context )
 
