fdd35f6bd2a1dfd1ad005a70635df00a262d55e8
“Moggach” <acunnane13@gmail.com>
2021-12-22 15:17:19 +0000

    connecting postgres db

--- tvtracker/settings.py
+++ tvtracker/settings.py
@@ -67,8 +67,11 @@ WSGI_APPLICATION = 'tvtracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tvtrackerdb',
+        'USER': 'postgres',
+        'PASSWORD': "Harry",
+        'HOST': 'localhost'
     }
 }
 
