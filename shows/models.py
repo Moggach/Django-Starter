2ce4ba5449508b9ddc8f630e3ea13e303a31e337
“Moggach” <acunnane13@gmail.com>
2021-12-23 12:04:13 +0000

    adding date aired field to db and changing name of rows to show title

--- shows/models.py
+++ shows/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.db.models.fields import DateField
+from datetime import date
+
 
 class Shows(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/', blank=True)
     description = models.TextField(blank=True)
     watched = models.BooleanField(default=False)
+    date_aired = models.DateField(default=date.today)
+    def __str__(self):
+        return self.name
+   
