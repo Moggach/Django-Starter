ec220fb5ef8d982504aa335a9c2ed3d2933e0cc4
“Moggach” <acunnane13@gmail.com>
2021-12-23 15:45:06 +0000

    readding link to local settings file

--- tvtracker/settings.py
+++ tvtracker/settings.py
@@ -124,3 +124,7 @@ IMAGES_URL = '/images/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
